cb3ac7c80d779e36a331f6d91a480c29e9bb08ac
casimirwilliam <casimir.william@gmail.com>
2015-10-12 22:26:42 -0700

    let see if this works

---
+++ Recoding2.R
@@ -0,0 +1,11 @@
+#May have fixed problem
+
+All2009<-recs2009_public
+
+library(plyr)
+
+library(reshape)
+
+#AIA_ZONE
+
+All2009$AIA_ZONE<- revalue(as.factor(All2009$AIA_Zone),c("1"="<2000CDD.>7000HDD","2"="<2000CDD.5500-7000HDD","3"="<2000CDD.4000-5499HDD","4"="<2000CDD.<4000DD","5"=">2000CDD.<4000HDD"))
